e9721aee3128e8bd2efed0e53b7214a1565aae05
Mehdi Lakhouane <leoamazigh@gmail.com>
2026-04-05 20:01:02 +0200

    Update interactable FSM logic

--- scripts/interactables/fsm/PlotState.cs
+++ scripts/interactables/fsm/PlotState.cs
@@ -5,7 +5,7 @@ namespace FSM;
 
 /// <summary>
 /// Specialized base class for all FarmPlot states.
-/// Provides a reference to the FarmPlot node.
+/// Provides a reference to the FarmPlot node and shared interact input handling.
 /// </summary>
 public abstract partial class PlotState : State
 {
@@ -14,5 +14,13 @@ public abstract partial class PlotState : State
     public override void _Ready()
     {
         _plot = GetOwner<FarmPlot>();
+    }
+
+    public virtual void Interact() {}
+
+    public override void HandleInput(InputEvent @event)
+    {
+        if (@event.IsActionPressed("interact"))
+            Interact();
     }
 }
